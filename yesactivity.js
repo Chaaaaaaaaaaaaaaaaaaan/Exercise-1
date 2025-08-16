b40963785f0b5fb76c28957db84a880286fa1d1f
Christian Dagoy <christian.dagoy@hcdc.edu.ph>
2025-08-16 20:46:30 +0800

    Add files via upload

---
+++ yesactivity.js
@@ -0,0 +1,67 @@
+// // Exercise 1
+
+// number 1
+var Myname = "Sana ako nalang baby please mahal na mahal naman kita";
+console.log(Myname);
+
+// // number 2
+// let age = 19;  
+
+// // number 3
+// const PI = 3.1416; 
+// console.log("PI =", PI);
+// // PI = 3.14;  
+
+// // number 4
+// let myAge = 50;  
+// let isStudent = myAge < 25; 
+// console.log("Age:", myAge);
+// console.log("Is Student?", isStudent);
+
+// // number 5
+// let person = {
+//     name: {
+//         first: "Christian",
+//         last: "Dagoy"
+//     },
+//     age: 19,
+//     contact: {
+//         email: "christian.dagoy@example.com",
+//         phone: "987-654-3210"
+//     },
+//     isStudent: true
+// };
+
+// console.log(person.name.first);  
+// console.log(person.contact.email); 
+
+// // number 6
+// let colors = ["Black", "grey", "white", "red", "Black"];
+// console.log(colors);
+
+// // number 1 (test 2)
+// let words = ["Valorant", "Bouquet", "Flowers", "honey"];
+// let transformedWords = [];
+
+// for (let i = 0; i < words.length; i++) {
+//     let reversedWord = "";
+//     for (let j = words[i].length - 1; j >= 0; j--) {
+//         reversedWord += words[i][j];
+//     }
+//     transformedWords.push(reversedWord.toUpperCase());
+// }
+// console.log(transformedWords);
+
+// // number 2 (test 2)
+// let numbers = [12, 45, 67, 23, 90, 32, 11, 9, 28];
+// let filteredNumbers = [];
+// let count = 0;
+
+// for (let num of numbers) {
+//     if (num % 2 === 0 && num > 20) {
+//         filteredNumbers.push(num);
+//         count++;
+//     }
+// }
+
+// console.log(`Found ${count} numbers: [${filteredNumbers.join(", ")}]`);
